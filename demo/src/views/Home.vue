8c63f7f2418d40ac4b54400e22b7b019bbe5b8c4
unknown <joel.salisbury@gmail.com>
2022-09-20 13:39:41 -0400

    what if it had a badass gallery

--- demo/src/views/Home.vue
+++ demo/src/views/Home.vue
@@ -1,17 +1,58 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-row no-gutters
+      class="mt-1"
+    >
+      <v-col
+        cols="12"
+        md="8">
+        <v-img
+          class="fill-height"
+          
+          :src  = "require('../assets/vehicle-demo/hero.jpg')"
+        />
+      </v-col>
+      <v-col
+        cols="12"
+        md="4">
+        <v-row no-gutters>
+        <v-col
+          v-for="n in 12"
+          :key="n"
+          cols="4"
+        >
+          <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            class="grey lighten-2"
+          >
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
